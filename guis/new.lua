4699c8f43a6c2d5603e6961d15417e10ce6267d6
soryed <brittaneymerritt71@gmail.com>
2025-10-12 21:52:12 -0700

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -5812,7 +5812,7 @@ mainapi:CreateCategory({
 mainapi:CreateCategory({
 	Name = 'AltFarm',
 	Icon = getcustomasset('ReVape/assets/new/af.png'),
-	Size = UDim2.fromOffset(420, 420)
+	Size = UDim2.fromOffset(20, 20)
 })
 mainapi.Categories.Main:CreateDivider('misc')
 
