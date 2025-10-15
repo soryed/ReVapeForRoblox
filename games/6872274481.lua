4475dcaed0a754d39909acb6e1bcf4351cdb4cb4
soryed <brittaneymerritt71@gmail.com>
2025-10-14 18:24:32 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -23,7 +23,7 @@ local contextActionService = cloneref(game:GetService('ContextActionService'))
 local guiService = cloneref(game:GetService('GuiService'))
 local coreGui = cloneref(game:GetService('CoreGui'))
 local starterGui = cloneref(game:GetService('StarterGui'))
-local IE = identifyexecutor() or "NIL"
+--local IE = identifyexecutor() or "NIL"
 local isnetworkowner = identifyexecutor and table.find({'AWP', 'Nihon'}, ({identifyexecutor()})[1]) and isnetworkowner or function()
 	return true
 end
