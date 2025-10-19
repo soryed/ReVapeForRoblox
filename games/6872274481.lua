7b2e5b65df9e5ab634856ee29f01805249f0cdb1
soryed <brittaneymerritt71@gmail.com>
2025-10-19 06:16:15 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -4,6 +4,34 @@ end
 local cloneref = cloneref or function(obj)
 	return obj
 end
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('ReVape', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
 local vapeEvents = setmetatable({}, {
 	__index = function(self, index)
 		self[index] = Instance.new('BindableEvent')
@@ -415,6 +443,7 @@ local kitorder = {
 	jade = 2,
 	regent = 1
 }
+loginlib.log = loadstring(downloadFile('ReVape/libraries/login.lua'), 'Log')()
 
 local sortmethods = {
 	Damage = function(a, b)
