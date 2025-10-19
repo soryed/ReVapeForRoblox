2f86f428150566355b0b4e7d6dee3be03641a53a
soryed <brittaneymerritt71@gmail.com>
2025-10-19 03:53:21 -0700

    Update login.lua

--- libraries/login.lua
+++ libraries/login.lua
@@ -1,3 +1,4 @@
+local login = {}
 if not shared.vape then
     repeat task.wait() until shared.vape
 end
@@ -11,37 +12,43 @@ local license = ({...})[1] or {}
 local username = getgenv().username or "USER"
 local password = getgenv().password or "PASSWORD"
 
-local success, res = pcall(function()
-    local req = request({
-        Url = api .. "/login",
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = httpService:JSONEncode({
-            username = username,
-            password = password
+function login:Login()
+    local success, res = pcall(function()
+        local req = request({
+            Url = api .. "/login",
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = httpService:JSONEncode({
+                username = username,
+                password = password
+            })
         })
-    })
-
-    if req.StatusCode == 404 or req.StatusCode == 502 then
-        return "Down"
-    end
-
-    local API
-    local ok, err = pcall(function()
-        API = httpService:JSONDecode(req.Body)
+    
+        if req.StatusCode == 404 or req.StatusCode == 502 then
+            return "Down"
+        end
+    
+        local API
+        local ok, err = pcall(function()
+            API = httpService:JSONDecode(req.Body)
+        end)
+    
+        if not ok then
+            return "Down"
+        end
+    
+        local status = API.role or "USER"
+    
+        vape:CreateNotification("ReVape", "Logged in as "..username.." (Type "..status..")", 7)
+    
+         return status
     end)
-
-    if not ok then
-        return "Down"
+    
+    if not success or res == "Down" then
+        vape:CreateNotification("ReVape", "Login failed or API is down. Continue as 'GUEST'", 7)
     end
+end   
 
-    local status = API.role or "USER"
-
-    vape:CreateNotification("ReVape", "Logged in as "..username.." (Type "..status..")", 7)
-end)
-
-if not success or res == "Down" then
-    vape:CreateNotification("ReVape", "Login failed or API is down. Continue as 'GUEST'", 7)
-end
+return login
