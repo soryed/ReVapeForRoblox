1b0896d3f558bef4aa64c0ab527e6e05753191ef
soryed <brittaneymerritt71@gmail.com>
2025-10-19 06:37:14 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9763,7 +9763,7 @@ run(function()
         Name = "KitRender",
         Tooltip = "Allows you to see everyone's kit during kit phase (5v5, Ranked)",
         Function = function(callback)
-	if role ~= "owner" and role ~= "coowner" and role ~= "admin"  and role ~= "friend" and role ~= "premium" then  notif("Vape", "You do not have the permission to use this", 10, "alert")  return end
+	
             if callback then
                 task.spawn(function()
                     local team2 = PlayerGui:WaitForChild("MatchDraftApp"):WaitForChild("DraftAppBackground"):WaitForChild("BodyContainer"):WaitForChild("Team2Column")
@@ -9854,7 +9854,7 @@ run(function()
 		Name = "MatchHistory",
 		Tooltip = 'Resets ur history',
 		Function = function(callback)
-		if role ~= "owner" and role ~= "coowner" and role ~= "admin"  and role ~= "friend" and role ~= "premium" then  notif("Vape", "You do not have the permission to use this", 10, "alert")  return end
+		
 			if callback then 
 				MatchHistory:Toggle()
 				local TeleportService = game:GetService("TeleportService")
@@ -9870,7 +9870,7 @@ run(function()
 		Name = "AutoBan",
 		Tooltip = 'Automatically bans a kit for you(5v5, ranked only)',
 		Function = function(callback)
-		if role ~= "owner" and role ~= "coowner" and role ~= "admin"  and role ~= "friend" and role ~= "premium" then  notif("Vape", "You do not have the permission to use this", 10, "alert")  return end
+		
 			if callback then 
 				AutoBan:Toggle()
 				  local kits = {"berserker", "hatterr", "flower_bee", "glacial_skater",'void_dragon','card','cat'}
@@ -10391,7 +10391,7 @@ run(function()
 	ViewProfiles = vape.Categories.Exploits:CreateModule({
 		Name = "ViewProfile",
 		Function = function(callback)
-	if role ~= "owner" and role ~= "coowner" and role ~= "admin"  and role ~= "friend" and role ~= "premium" then  notif("Vape", "You do not have the permission to use this", 10, "alert")  return end
+	
 			notif("Vape", "This function is not finish", 10,"alert")
 		end,
 		Tooltip = "This allows you to see other peoples profile"
