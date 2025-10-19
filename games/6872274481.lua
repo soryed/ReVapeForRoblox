dc6f7dc62efe7b67553c541b8810e8f5a7f00d65
soryed <brittaneymerritt71@gmail.com>
2025-10-19 05:32:31 -0700

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9759,7 +9759,7 @@ run(function()
         Name = "KitRender",
         Tooltip = "Allows you to see everyone's kit during kit phase (5v5, Ranked)",
         Function = function(callback)
-		if not shared.vape.role == "owner" or not shared.vape.role == "coowner" or not shared.vape.role == "admin" or not shared.vape.role == "friend" or not shared.vape.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
+		if not shared.role == "owner" or not shared.role == "coowner" or not shared.role == "admin" or not shared.role == "friend" or not shared.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
             if callback then
                 task.spawn(function()
                     local team2 = PlayerGui:WaitForChild("MatchDraftApp"):WaitForChild("DraftAppBackground"):WaitForChild("BodyContainer"):WaitForChild("Team2Column")
@@ -9850,7 +9850,7 @@ run(function()
 		Name = "MatchHistory",
 		Tooltip = 'Resets ur history',
 		Function = function(callback)
-			if not shared.vape.role == "owner" or not shared.vape.role == "coowner" or not shared.vape.role == "admin" or not shared.vape.role == "friend" or not shared.vape.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
+			if not shared.role == "owner" or not shared.role == "coowner" or not shared.role == "admin" or not shared.role == "friend" or not shared.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
 			if callback then 
 				MatchHistory:Toggle()
 				local TeleportService = game:GetService("TeleportService")
@@ -9866,7 +9866,7 @@ run(function()
 		Name = "AutoBan",
 		Tooltip = 'Automatically bans a kit for you(5v5, ranked only)',
 		Function = function(callback)
-			if not shared.vape.role == "owner" or not shared.vape.role == "coowner" or not shared.vape.role == "admin" or not shared.vape.role == "friend" or not shared.vape.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
+			if not shared.role == "owner" or not shared.role == "coowner" or not shared.role == "admin" or not shared.role == "friend" or not shared.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
 			if callback then 
 				AutoBan:Toggle()
 				  local kits = {"berserker", "hatterr", "flower_bee", "glacial_skater",'void_dragon','card','cat'}
@@ -10047,7 +10047,7 @@ run(function()
 		Name = "Desync",
 		Function = function()
 			if not enabled.Enabled then vape:CreateNotification('ReVape', "Ignored, You do not have the setting on to use this module",5,"warning") return end
-			if not shared.vape.role == "owner" or not shared.vape.role == "coowner" or not shared.vape.role == "admin" or not shared.vape.role == "friend" or not shared.vape.role == "premium" then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
+			if not shared.role == "owner" or not shared.role == "coowner" or not shared.role == "admin" or not shared.role == "friend" or not shared.role == "premium" then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
 
 		setfflag("FFlagDebugGraphicsPreferD3D11","True")
 		setfflag("FLogNetwork","7")
@@ -10387,7 +10387,7 @@ run(function()
 	ViewProfiles = vape.Categories.Exploits:CreateModule({
 		Name = "ViewProfile",
 		Function = function(callback)
-		if not shared.vape.role == "owner" or not shared.vape.role == "coowner" or not shared.vape.role == "admin" or not shared.vape.role == "friend" or not shared.vape.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
+		if not shared.role == "owner" or not shared.role == "coowner" or not shared.role == "admin" or not shared.role == "friend" or not shared.role == "premium"  then notif("Vape", "You do not have the permission to use this", 10,"alert") return end
 			notif("Vape", "This function is not finish", 10,"alert")
 		end,
 		Tooltip = "This allows you to see other peoples profile"
