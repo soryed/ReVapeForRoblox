f3a9c1b6eea5da3ae148b4bb5f52c2a8c32efb3e
soryed <brittaneymerritt71@gmail.com>
2025-11-02 03:27:06 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,6 +29,170 @@ local cloneref = cloneref or function(obj)
 	return obj
 end
 local playersService = cloneref(game:GetService('Players'))
+local gethui = gethui or function() return game:GetService('Players').LocalPlayer.PlayerGui end
+local tweenService = game:GetService('TweenService')
+
+local gui : ScreenGui = Instance.new('ScreenGui', gethui())
+gui.Enabled = true
+
+local stages = {
+	UDim2.new(0, 50, 1, 0),
+	UDim2.new(0, 100, 1, 0),
+	UDim2.new(0, 160, 1, 0),
+	UDim2.new(0, 200, 1, 0),
+	UDim2.new(0, 240, 1, 0)
+}
+
+local createinstance = function(class, properties)
+	local res = Instance.new(class)
+	
+	for property, value in properties do
+		res[property] = value
+	end
+
+	return res
+end
+
+if gui.Enabled then
+	createinstance('ImageLabel', {
+		Name = 'Main',
+		Parent = gui,
+		BackgroundTransparency = 1,
+		Size = UDim2.fromOffset(685, 399),
+		Position = UDim2.fromScale(0.5, 0.5),
+		AnchorPoint = Vector2.new(0.5, 0.5),
+		ScaleType = Enum.ScaleType.Fit,
+		Image = 'rbxassetid://93496634716737'
+	})
+
+	-- EXIT BUTTON --
+
+	createinstance('ImageButton', {
+		Name = 'Exit',
+		Parent = gui.Main,
+		BackgroundTransparency = 1,
+		Position = UDim2.fromOffset(624, 23),
+		Size = UDim2.fromOffset(40, 30),
+		AutoButtonColor = false,
+		ImageColor3 = Color3.fromRGB(34, 33, 34),
+		Image = 'rbxassetid://110629770884920',
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	createinstance('ImageLabel', {
+		Name = 'Icon',
+		Parent = gui.Main.Exit,
+		BackgroundTransparency = 1,
+		Position = UDim2.new(0, 10, 0.5, 0),
+		Size = UDim2.fromOffset(16, 16),
+		AnchorPoint = Vector2.new(0, 0.5),
+		ImageTransparency = 0.4,
+		ImageColor3 = Color3.new(1, 1, 1),
+		Image = 'rbxassetid://128518278755224',
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	-- MINIMIZE BUTTON --
+
+	createinstance('ImageButton', {
+		Name = 'Minimize',
+		Parent = gui.Main,
+		BackgroundTransparency = 1,
+		Position = UDim2.fromOffset(582, 23),
+		Size = UDim2.fromOffset(40, 30),
+		AutoButtonColor = false,
+		ImageColor3 = Color3.fromRGB(34, 33, 34),
+		Image = 'rbxassetid://133363055871405',
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	createinstance('ImageLabel', {
+		Name = 'Icon',
+		Parent = gui.Main.Minimize,
+		BackgroundTransparency = 1,
+		Position = UDim2.new(0, 14, 0.5, 0),
+		Size = UDim2.fromOffset(16, 16),
+		AnchorPoint = Vector2.new(0, 0.5),
+		ImageTransparency = 0.4,
+		ImageColor3 = Color3.new(1, 1, 1),
+		Image = 'rbxassetid://83568668289707',
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	-- VAPE LOGO --
+
+	createinstance('ImageLabel', {
+		Name = 'textvape',
+		Parent = gui.Main,
+		AnchorPoint = Vector2.new(0.48, 0.31),
+		BackgroundTransparency = 1,
+		Position = UDim2.fromScale(0.48, 0.31),
+		Size = UDim2.fromOffset(70, 70),
+		Image = 'rbxassetid://84228868064393',
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	createinstance('ImageLabel', {
+		Name = 'version',
+		Parent = gui.Main.textvape,
+		BackgroundTransparency = 1,
+		Position = UDim2.fromScale(1, 0.3),
+		Size = UDim2.fromOffset(29, 29),
+		Image = 'rbxassetid://138794287840926',
+		ImageColor3 = Color3.fromRGB(98, 198, 158),
+		ScaleType = Enum.ScaleType.Fit
+	})
+
+	-- LOAD BAR --
+
+	createinstance('Frame', {
+		Name = 'loadbar',
+		Parent = gui.Main,
+		AnchorPoint = Vector2.new(0.5, 0.53),
+		BackgroundColor3 = Color3.fromRGB(20, 20, 20),
+		BorderSizePixel = 0,
+		Position = UDim2.fromScale(0.5, 0.53),
+		Size = UDim2.fromOffset(240, 6)
+	})
+
+	createinstance('Frame', {
+		Name = 'fullbar',
+		Parent = gui.Main.loadbar,
+		BackgroundColor3 = Color3.fromRGB(3, 102, 79),
+		BorderSizePixel = 0,
+		Size = UDim2.new(0, 0, 1, 0),
+		ZIndex = 2
+	})
+
+	-- ACTION TEXT --
+
+	createinstance('TextLabel', {
+		Name = 'action',
+		Parent = gui.Main,
+		BackgroundTransparency = 1,
+		Position = UDim2.fromScale(0.353284657, 0.556391001),
+		Size = UDim2.fromOffset(200, 15),
+		Font = Enum.Font.Arial,
+		Text = '',
+		TextColor3 = Color3.new(1, 1, 1),
+		TextSize = 12,
+		TextTransparency = 0.3
+	})
+
+	Instance.new('UICorner', gui.Main.loadbar)
+	Instance.new('UICorner', gui.Main.loadbar.fullbar)
+	Instance.new('UIScale', gui.Main).Scale = math.max(gui.AbsoluteSize.X / 1920, 0.485)
+end;
+
+makestage = function(stage, package)
+	pcall(function()
+		tweenService:Create(gui.Main.loadbar.fullbar, TweenInfo.new(0.4, Enum.EasingStyle.Quad), {
+			Size = stages[stage]
+		}):Play()
+		gui.Main.action.Text = package or ''
+	end)
+end
+
 
 local function downloadFile(path, func)
 	if not isfile(path) then
