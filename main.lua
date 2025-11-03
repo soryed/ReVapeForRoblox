2c0b130db74e5e5706214d458afff672f930b499
soryed <brittaneymerritt71@gmail.com>
2025-11-03 02:52:08 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -329,17 +329,7 @@ if IVM then
 task.wait(1.2)
 				makestage(5, 'restarting...', .8)
 		task.wait(.95)
-	for _, v in gui:GetDescendants() do
-					for __, prop in ipairs({'BackgroundTransparency', 'ImageTransparency', 'TextTransparency'}) do
-						task.spawn(function()
-							pcall(function()
-								tweenService:Create(v, TweenInfo.new(1, Enum.EasingStyle.Quad), {
-									[prop] = 1
-								}):Play()
-							end)
-						end)
-					end
-				end
+gui:Destroy()
 				shared.vapereload = true
 		if shared.VapeDeveloper then
 
