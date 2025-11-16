9b7391ca1e1598d40fd356dd449c8851397b7530
soryed <brittaneymerritt71@gmail.com>
2025-11-16 04:48:43 -0800

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -20,7 +20,7 @@ local mainapi = {
 	Scale = {Value = 1},
 	ThreadFix = setthreadidentity and true or false,
 	ToggleNotifications = {},
-	Version = '0.51',
+	Version = '0.52',
 	Discord = "@ye40",
 	role = "",
 	user = "",
