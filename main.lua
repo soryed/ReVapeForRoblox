5b7ca20ff86fccdc98c26e8bb58a11140ce7042e
jazzy <brittaneymerritt71@gmail.com>
2025-12-05 23:50:41 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,9 @@
 repeat task.wait() until game:IsLoaded()
 if shared.vape then shared.vape:Uninject() end
 
+-- why do exploits fail to implement anything correctly? Is it really that hard?
 if identifyexecutor then
-	if table.find({'Argon'}, ({identifyexecutor()})[1]) then
+	if table.find({'Argon', 'Wave'}, ({identifyexecutor()})[1]) then
 		getgenv().setthreadidentity = nil
 	end
 end
@@ -11,7 +12,7 @@ local vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then
-		vape:CreateNotification('Onyx - main', 'Failed to load : '..err, 30, 'alert')
+		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert')
 	end
 	return res
 end
@@ -42,45 +43,8 @@ local function downloadFile(path, func)
 	end
 	return (func or readfile)(path)
 end
-local function serializeValue(v)
-    local t = type(v)
-    if t == "string" then
-        return ("%q"):format(v)
-    elseif t == "number" or t == "boolean" or t == "nil" then
-        return tostring(v)
-    elseif t == "table" then
-        local isArray = true
-        local max = 0
-        for k,_ in pairs(v) do
-            if type(k) ~= "number" then isArray = false break end
-            if k > max then max = k end
-        end
-        if isArray then
-            local parts = {}
-            for i = 1, max do
-                table.insert(parts, serializeValue(v[i]))
-            end
-            return "{" .. table.concat(parts, ",") .. "}"
-        else
-            local parts = {}
-            for k,val in pairs(v) do
-                local key
-                if type(k) == "string" and k:match("^[_%a][_%w]*$") then
-                    key = k
-                else
-                    key = "[" .. serializeValue(k) .. "]"
-                end
-                table.insert(parts, key .. "=" .. serializeValue(val))
-            end
-            return "{" .. table.concat(parts, ",") .. "}"
-        end
-    else
-        return ("%q"):format(tostring(v))
-    end
-end
 
 local function finishLoading()
-
 	vape.Init = nil
 	vape:Load()
 	task.spawn(function()
@@ -90,50 +54,31 @@ local function finishLoading()
 		until not vape.Loaded
 	end)
 
-
-	local teleportedServers
-		vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
-				
+--[[	local teleportedServers
+	vape:Clean(playersService.LocalPlayer.OnTeleport:Connect(function()
 		if (not teleportedServers) and (not shared.VapeIndependent) then
 			teleportedServers = true
-			local teleportScript = [[
+			local teleportScript =
 				shared.vapereload = true
 				if shared.VapeDeveloper then
-
 					loadstring(readfile('ReVape/loader.lua'), 'loader')()
 				else
-
-					loadstring(game:HttpGet('https://raw.githubusercontent.com/soryed/OynxVAPEv4/'..readfile('ReVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
+					loadstring(game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/loader.lua', true), 'loader')()
 				end
-			]]
+		
 			if shared.VapeDeveloper then
-
 				teleportScript = 'shared.VapeDeveloper = true\n'..teleportScript
-						
 			end
-			    if getgenv().username ~= nil then
-        teleportScript = "getgenv().username = " .. serializeValue(getgenv().username) .. "\n" .. teleportScript
-    end
-    if getgenv().password ~= nil then
-        teleportScript = "getgenv().password = " .. serializeValue(getgenv().password) .. "\n" .. teleportScript
-    end
-    if getgenv().SLS then
-        teleportScript = "getgenv().SLS = true\n" .. teleportScript
-    end
-    if getgenv().SkipLoadingScreen then
-        teleportScript = "getgenv().SkipLoadingScreen = true\n" .. teleportScript
-    end
-	if getgenv().TestMode then
-        teleportScript = "getgenv().TestMode = true\n" .. teleportScript
-    end
-    if shared.VapeCustomProfile then
-        teleportScript = "shared.VapeCustomProfile = " .. serializeValue(shared.VapeCustomProfile) .. "\n" .. teleportScript
-    end
-
+			if shared.VapeCustomProfile then
+				teleportScript = 'shared.VapeCustomProfile = "'..shared.VapeCustomProfile..'"\n'..teleportScript
+			end
+			if getgenv().username then
+				teleportScript = 'getgenv().username'
+			end
 			vape:Save()
 			queue_on_teleport(teleportScript)
 		end
-	end))
+	end))--]]
 
 	if not shared.vapereload then
 		if not vape.Categories then return end
@@ -145,7 +90,6 @@ local function finishLoading()
 	end
 end
 
-
 if not isfile('ReVape/profiles/gui.txt') then
 	writefile('ReVape/profiles/gui.txt', 'new')
 end
@@ -154,7 +98,7 @@ local gui = readfile('ReVape/profiles/gui.txt')
 if not isfolder('ReVape/assets/'..gui) then
 	makefolder('ReVape/assets/'..gui)
 end
-vape = loadstring(downloadFile('ReVape/guis/new.lua'), 'gui')()
+vape = loadstring(downloadFile('ReVape/guis/'..gui..'.lua'), 'gui')()
 shared.vape = vape
 
 if not shared.VapeIndependent then
