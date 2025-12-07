672b2ff4bc0f24107d56b545093c68a4700f70ed
jazzy <brittaneymerritt71@gmail.com>
2025-12-07 14:11:45 -0800

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -20,7 +20,7 @@ local mainapi = {
 	Scale = {Value = 1},
 	ThreadFix = setthreadidentity and true or false,
 	ToggleNotifications = {},
-	Version = '0.65',
+	Version = '0.66',
 	Discord = "@ye40",
 	role = "",
 	user = "",
