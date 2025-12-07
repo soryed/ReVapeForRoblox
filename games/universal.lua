aa552c5d4c283c88ba963106b920866d65258763
jazzy <brittaneymerritt71@gmail.com>
2025-12-07 13:27:10 -0800

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -627,16 +627,7 @@ run(function()
 			end
 
 			if whitelist.textdata ~= whitelist.olddata then
-				if whitelist.data.Announcement.expiretime > os.time() then
-					local targets = whitelist.data.Announcement.targets
-					targets = targets == 'all' and {tostring(lplr.UserId)} or targets:split(',')
 
-					if table.find(targets, tostring(lplr.UserId)) then
-						local hint = Instance.new('Hint')
-						hint.Text = 'VAPE ANNOUNCEMENT: '..whitelist.data.Announcement.text
-						hint.Parent = workspace
-						game:GetService('Debris'):AddItem(hint, 20)
-					end
 				end
 				whitelist.olddata = whitelist.textdata
 				pcall(function()
