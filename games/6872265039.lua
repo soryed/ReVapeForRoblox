0e3125cf4a4953af06546ad6550fe9765f0d9636
jazzy <brittaneymerritt71@gmail.com>
2025-12-13 12:52:22 -0800

    Update 6872265039.lua

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -1779,7 +1779,7 @@ run(function()
 			if not callback then return
 			if Type.Value == "Known" then
 				OnlineMods(Mod.Value)
-			elseif Type.Value == "Unknown" then
+			else
 				OnlineMods("nns")
 			end
 		end,
