81ccafa1cbac20b1c8d8c91abc3590d8af18d88b
jazzy <brittaneymerritt71@gmail.com>
2025-12-14 12:09:18 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -15003,7 +15003,6 @@ if getgenv().TestMode or role == "owner" or role == "coowner" then
 				root.AssemblyLinearVelocity = Vector3.new(direction.X * speed,0,direction.Z * speed)
 			end))
 		})
-	end)
 end
 
 
