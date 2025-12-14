c8ac513892074e940ab60825d11379dfa66eb39c
jazzy <brittaneymerritt71@gmail.com>
2025-12-14 12:22:44 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -14914,7 +14914,7 @@ if getgenv().TestMode or role == "owner" or role == "coowner" then
 				local direction = delta.Unit
 				local speed = getSpeed() + JumpSpeed - 1.5
 
-				root.AssemblyLinearVelocity =Vector3.new(direction.X * speed, math.max(root.AssemblyLinearVelocity.Y, 2), direction.Z * speed)
+				root.AssemblyLinearVelocity = root.AssemblyLinearVelocity + direction * speed + Vector3.new(0, root.AssemblyLinearVelocity.Y, 0)
 			end))
 		end 
 	})
