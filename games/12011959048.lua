3cf97541ccf263f208a5ccbe7743086483d3be1d
jazzy <brittaneymerritt71@gmail.com>
2025-12-20 16:50:28 -0800

    Update 12011959048.lua

--- games/12011959048.lua
+++ games/12011959048.lua
@@ -2,6 +2,8 @@ local vape = shared.vape
 local loadstring = function(...)
 	local res, err = loadstring(...)
 	if err and vape then 
+				print(debug.traceback())
+
 		vape:CreateNotification('Vape', 'Failed to load : '..err, 30, 'alert') 
 	end
 	return res
