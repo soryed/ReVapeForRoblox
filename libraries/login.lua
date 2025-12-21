cd7702dcbf416d105be4aad04802a75e5e7635cf
jazzy <brittaneymerritt71@gmail.com>
2025-12-20 17:00:05 -0800

    Update login.lua

--- libraries/login.lua
+++ libraries/login.lua
@@ -1,18 +1,6 @@
 local login = {}
 
-local function downloadFile(path, func)
-    if not isfile(path) then
-        createDownloader(path)
-        local suc, res = pcall(function()
-            return game:HttpGet("https://raw.githubusercontent.com/soryed/ReVapeForRoblox/"..
-                readfile("ReVape/profiles/commit.txt").."/"..select(1, path:gsub("ReVape/", "")), true)
-        end)
-        if not suc or res == "404: Not Found" then error(res) end
-        if path:find(".lua") then res = "\n"..res end
-        writefile(path, res)
-    end
-    return (func or readfile)(path)
-end
+
 
 if not shared.vape then repeat task.wait() until shared.vape end
 local cloneref = cloneref or function(obj) return obj end
@@ -27,6 +15,36 @@ local SignupBase = ApiBase.."signup"
 local HwidResetBase = ApiBase.."reset-hwid"
 local username = getgenv().username or "GUEST"
 local password = getgenv().password or "PASSWORD"
+
+local loadstring = function(...)
+	local res, err = loadstring(...)
+	if err and vape then
+		vape:CreateNotification('Onyx', 'Failed to load : '..err, 30, 'alert')
+	end
+	return res
+end
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+
+local function downloadFile(path, func)
+	if not isfile(path) then
+		local suc, res = pcall(function()
+			return game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
+		end)
+		if not suc or res == '404: Not Found' then
+			error(res)
+		end
+		if path:find('.lua') then
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+		end
+		writefile(path, res)
+	end
+	return (func or readfile)(path)
+end
 
 local HWID_PATH = "ReVape/accounts/hwid.txt"
 local GenLib = loadstring(downloadFile("ReVape/libraries/Generator.lua"), "Generator")()
