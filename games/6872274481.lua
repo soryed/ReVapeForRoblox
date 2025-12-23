fff79ec30ba2e81eb6fbec1149cda44a7f431fc7
jazzy <brittaneymerritt71@gmail.com>
2025-12-23 00:10:44 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -17473,13 +17473,23 @@ run(function()
 
 	local function CreatePlayerList()
 		target = ""
-		table.clear(real_list)
-		for _, v in pairs(playersService:GetPlayers()) do
-			if v == lplr then
-				continue
+		if real_list == nil then
+			for _, v in pairs(playersService:GetPlayers()) do
+				if v == lplr then
+					continue
+				end
+				table.insert(real_list, v.Name)
 			end
-			table.insert(real_list, v.Name)
+				else
+							table.clear(real_list)
+			for _, v in pairs(playersService:GetPlayers()) do
+				if v == lplr then
+					continue
+				end
+				table.insert(real_list, v.Name)
+			end
 		end
+
 	end
 	CreatePlayerList()
 	Invites = vape.Categories.Exploits:CreateModule({
