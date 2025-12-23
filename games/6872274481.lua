8897230fa3f5936b11fb8dd27bff54613de5bfc2
jazzy <brittaneymerritt71@gmail.com>
2025-12-23 01:34:10 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -17560,7 +17560,6 @@ run(function()
 								end
 							elseif Dashes.Value == 2 then
 								task.wait(0.36)
-								setclipboard(httpService:JSONEncode(bedwars.Client:Get("ElectricDash")))
 								if bedwars.AbilityController:canUseAbility("ELECTRIC_DASH") then
 									vape:CreateNotification("AutoWin", "Dashing to bypass anti cheat!", 1)
 									bedwars.AbilityController:useAbility('ELECTRIC_DASH')
