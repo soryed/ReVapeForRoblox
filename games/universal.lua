c7e7a48d11125329f1143c7e0f6973a0c54161a2
jazzy <brittaneymerritt71@gmail.com>
2025-12-25 13:00:33 -0800

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -14,7 +14,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/qyroke2/ReVapeForRoblox/'..readfile('ReVape/profiles/commit.txt')..'/'..select(1, path:gsub('ReVape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
@@ -8038,7 +8038,7 @@ task.spawn(function()
                 end)
             else
                 pcall(function()
-                    loadstring(game:HttpGet('https://raw.githubusercontent.com/soryed/ReVapeForRoblox/main/loader.lua', true))()
+                    loadstring(game:HttpGet('https://raw.githubusercontent.com/qyroke2/ReVapeForRoblox/main/loader.lua', true))()
                 end)
             end
 
@@ -8891,8 +8891,8 @@ task.spawn(function()
 				end 
 				-- made like this
 local Injection = string.format([[ 
--- Inject this for now on, Created by Soryed 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/soryed/ReVapeForRoblox/main/NewMainScript.lua", true))({
+-- Inject this for now on, Created by qyroke2 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qyroke2/ReVapeForRoblox/main/NewMainScript.lua", true))({
     username = "%s",
     password = "%s"
 })
