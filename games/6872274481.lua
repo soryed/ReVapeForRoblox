635889d36698879878f973789612a284b0231b4c
jazzy <brittaneymerritt71@gmail.com>
2026-01-02 10:59:29 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -9800,6 +9800,7 @@ run(function()
 															local calc = prediction.SolveTrajectory(pos, projSpeed, gravity, actualRoot.Position, actualRoot.Velocity, workspace.Gravity, v.Character.Humanoid.HipHeight, v.Character.Humanoid.Jumping and 42.6 or nil, rayCheck)
 															if calc then
 																local slot = getObjSlot(projectile)
+																print(slot,projectile)																																																																																																																																					
 																local switched = switchHotbar(slot)
 																task.spawn(function()
 																	local dir, id = CFrame.lookAt(pos, calc).LookVector, httpService:GenerateGUID(true)
