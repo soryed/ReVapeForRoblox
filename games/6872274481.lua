490f9d8ef7b1f63ad1af96b2d3eabdecc600c036
jazzy <brittaneymerritt71@gmail.com>
2026-01-04 08:28:44 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -12235,7 +12235,6 @@ run(function()
         Max = 10,
         Default = 5,
 		Visible = false
-        Suffix = function(val) return val == 1 and "stud" or "studs" end
     })
 	CastChecks = BetterKaida:CreateToggle({
 		Name = "Check Checks",
