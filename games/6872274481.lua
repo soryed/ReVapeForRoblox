fcfdeebc847531bff91c296b75f75ae381fa1535
jazzy <brittaneymerritt71@gmail.com>
2026-01-05 05:30:26 -0800

    Update 6872274481.lua

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -1228,13 +1228,6 @@ run(function()
 		})
 	end))
 
-			vape:Clean(bedwars.ZapNetworking.placeBlockRemoteZap.On(function(...)
-		print(...)
-		print(httpService:JSONEncode(...))
-		--[[vapeEvents.placeBlockEvent:Fire({
-
-		})-]]
-	end))
 		
 	for _, event in {'PlaceBlockEvent', 'BreakBlockEvent'} do
 		vape:Clean(bedwars.ZapNetworking[event..'Zap'].On(function(...)
